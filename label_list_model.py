6eaf8343eeaa2c75dc902c0d8d120311abf0cb14
Naoto Kondo <cgigcp3yqt@gmail.com>
2021-12-01 18:24:53 +0900

    label icon

--- label_list_model.py
+++ label_list_model.py
@@ -1,4 +1,5 @@
 from PySide6.QtCore import Qt, QAbstractListModel, QModelIndex
+from PySide6.QtGui import QPixmap, QIcon
 from app import *
 
 
@@ -21,6 +22,13 @@ class LabelListModel(QAbstractListModel):
 
         if role == Qt.DisplayRole:
             return self.app.labels[index.row()].id
+        elif role == Qt.DecorationRole:
+            return self.create_color_icon(self.app.labels[index.row()].color)
 
         else:
             return None
+
+    def create_color_icon(self, color):
+        pixmap = QPixmap(64, 64)
+        pixmap.fill(color)
+        return QIcon(pixmap)
