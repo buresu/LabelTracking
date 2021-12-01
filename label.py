4ff637ac4cb41426fd08c62260dce92fba076d4d
Naoto Kondo <cgigcp3yqt@gmail.com>
2021-12-01 18:14:15 +0900

    cleanup

--- label.py
+++ label.py
@@ -1,12 +1,13 @@
 from PySide6.QtCore import Qt, QRectF
 
 
-class Label():
-    def __init__(self, parent=None):
-        self.id = "label1"
-        self.color = Qt.red
+class Label(object):
+    def __init__(self, id='label1', color=Qt.red, parent=None):
+        self.id = id
+        self.color = color
 
-class LabelArea():
+
+class LabelArea(object):
     def __init__(self, parent=None):
         self.id = ""
         self.rect = QRectF()
