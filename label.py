bc49a12fbc0565b0c650e6d2916e3b939ae38ccd
Naoto Kondo <cgigcp3yqt@gmail.com>
2021-12-05 05:42:47 +0900

    fix label color

--- label.py
+++ label.py
@@ -1,10 +1,11 @@
-from PySide6.QtCore import Qt, QRectF, QPointF, QColor
+from PySide6.QtCore import Qt, QRectF, QPointF
+from PySide6.QtGui import QColor
 
 
 class Label(object):
     def __init__(self, id='label1', color=Qt.red, parent=None):
         self.id = id
-        self.color = color
+        self.color = QColor(color)
 
     def serialize(self):
         obj = dict()
