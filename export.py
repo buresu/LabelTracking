34cb91d540dd817e34c9be65e61202d846c2c4d4
Naoto Kondo <cgigcp3yqt@gmail.com>
2021-12-05 11:00:43 +0900

    speed up

--- export.py
+++ export.py
@@ -1,7 +1,8 @@
 import os
 import glob
+import shutil
 import base64
-import cv2 as cv
+from PySide6.QtGui import QImageReader
 from app import *
 
 
@@ -23,46 +24,43 @@ class Exporter(object):
         for filename in filenames:
             basename = os.path.basename(filename)
             print(basename)
-            f = QFile(os.path.join(dirname, basename))
-            if f.open(QFile.WriteOnly):
-                f2 = QFile(os.path.join(self.app.output_dir, basename))
-                if f2.open(QFile.ReadOnly):
-                    try:
-                        json = QJsonDocument.fromJson(f2.readAll()).object()
-                        label_areas = [LabelArea.deserialized(
-                            obj) for obj in json['labelAreas']]
-                        image_path = os.path.splitext(os.path.join(
-                            self.app.output_dir, basename))[0] + '.jpg'
-                        image_out_path = os.path.splitext(os.path.join(
-                            dirname, basename))[0] + '.jpg'
-                        image = cv.imread(image_path)
-                        json = dict()
-                        json['version'] = '4.6.1'
-                        json['flags'] = {}
-                        json['imageWidth'] = image.shape[1]
-                        json['imageHeight'] = image.shape[0]
-                        json['imagePath'] = os.path.relpath(
-                            image_out_path, dirname)
-                        ret, data = cv.imencode('.jpg', image)
-                        json['imageData'] = base64.b64encode(
-                            data.tobytes()).decode('utf-8')
-                        areas = []
-                        for area in label_areas:
-                            area_json = dict()
-                            area_json['label'] = area.id
-                            area_json['group_id'] = None
-                            area_json['flags'] = {}
-                            area_json['shape_type'] = 'rectangle'
-                            area_json['points'] = [[area.key_points[0].x(), area.key_points[0].y()], [
-                                area.key_points[1].x(), area.key_points[1].y()]]
-                            areas.append(area_json)
-                        json['shapes'] = areas
-                        f.write(QJsonDocument.fromVariant(json).toJson())
-                        cv.imwrite(image_out_path, image)
-                    except:
-                        print('Export eccor')
-                    f2.close()
-                f.close()
+            frame_data = self.read_data(
+                os.path.join(self.app.output_dir, basename))
+            try:
+                json = QJsonDocument.fromJson(frame_data).object()
+                label_areas = [LabelArea.deserialized(
+                    obj) for obj in json['labelAreas']]
+                image_path = os.path.splitext(os.path.join(
+                    self.app.output_dir, basename))[0] + '.jpg'
+                image_out_path = os.path.splitext(os.path.join(
+                    dirname, basename))[0] + '.jpg'
+                image = QImageReader(image_path)
+                json = dict()
+                json['version'] = '4.6.1'
+                json['flags'] = {}
+                json['imageWidth'] = image.size().width()
+                json['imageHeight'] = image.size().height()
+                json['imagePath'] = os.path.relpath(
+                    image_out_path, dirname)
+                image_data = self.read_data(image_path)
+                json['imageData'] = base64.b64encode(
+                    image_data).decode('utf-8')
+                areas = []
+                for area in label_areas:
+                    area_json = dict()
+                    area_json['label'] = area.id
+                    area_json['group_id'] = None
+                    area_json['flags'] = {}
+                    area_json['shape_type'] = 'rectangle'
+                    area_json['points'] = [[area.key_points[0].x(), area.key_points[0].y()], [
+                        area.key_points[1].x(), area.key_points[1].y()]]
+                    areas.append(area_json)
+                json['shapes'] = areas
+                self.write_data(os.path.join(dirname, basename),
+                                QJsonDocument.fromVariant(json).toJson())
+                shutil.copyfile(image_path, image_out_path)
+            except:
+                print('Export eccor')
 
     def read_data(self, filename):
         f = QFile(filename)
@@ -72,7 +70,7 @@ class Exporter(object):
             return data
         return b''
 
-    def save_data(self, filename, data):
+    def write_data(self, filename, data):
         f = QFile(filename)
         if f.open(QFile.WriteOnly):
             f.write(data)
