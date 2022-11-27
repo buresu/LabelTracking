b48174268cac8209eecd2a1c1bb2cdeef8c2805c
Naoto Kondo <cgigcp3yqt@gmail.com>
2022-11-27 13:27:15 +0900

    update version to 0.2.0

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ class App(QObject, metaclass=Singleton):
     def __init__(self):
         super(App, self).__init__()
 
-        self.version = '0.1.1'
+        self.version = '0.2.0'
 
         self.output_dir = os.path.abspath('./output')
 
